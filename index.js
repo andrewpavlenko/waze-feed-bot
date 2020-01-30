a7e8ae8f13e47a459c55d424b01be6796c40a97a
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-01-30 16:57:33 +0200

    Implemented data processing

---
+++ index.js
@@ -0,0 +1,128 @@
+const https = require('https');
+
+const options = {
+    // Konotop
+    // areaBounds: {
+    //     left: 33.140052,
+    //     right: 33.255099,
+    //     top: 51.273409,
+    //     bottom: 51.184777
+    // },
+    // Sumy
+    // areaBounds: {
+    //     left: 34.743942,
+    //     right: 34.869877,
+    //     top: 50.946607,
+    //     bottom: 50.873301
+    // },
+    // Mykolaiv
+    areaBounds: {
+        left: 31.937925,
+        right: 32.062687,
+        top: 46.982323,
+        bottom: 46.923851
+    },
+    requestUrl: 'https://www.waze.com/row-rtserver/web/TGeoRSS?tk=community&format=JSON',
+    updateInterval: minutes(5)
+};
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+const dbDefaults = {
+    processedAlerts: [],
+    users: []
+};
+
+db.defaults(dbDefaults)
+  .write()
+
+startWatcher();
+
+function startWatcher() {
+    log('starting watcher');
+    let url = addBoundsToUrl(options.requestUrl);
+    https.get(url, res => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                processData(parsedData);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    });
+}
+
+function processData(data) {
+    let { users, alerts } = data;
+    if (alerts) {
+        processAlerts(alerts);
+    }
+    if (users) {
+        processUsers(users);
+    }
+}
+
+function processAlerts(alerts) {
+    log('processing alerts');
+    let processedAlerts = db.get('processedAlerts');
+    alerts.forEach(alert => {
+        let { uuid } = alert;
+        let isProcessed = processedAlerts.value().includes(uuid);
+        
+        if (!isProcessed) {
+            // Notify alert and then add to database
+            processedAlerts.push(uuid).write();
+        }
+    });
+}
+
+function processUsers(users) {
+    log('processing users');
+    let dbUsers = db.get('users');
+    let timestamp = Date.now();
+
+    users.forEach(user => {
+        let matchedUser = dbUsers.find({ id: user.id });
+
+        if (matchedUser.value() !== undefined) {
+            matchedUser.assign({ lastSeen: timestamp }).write();
+        } else {
+            let { id, mood, userName } = user;
+            dbUsers.push({
+                id,
+                mood,
+                userName,
+                lastSeen: timestamp
+            })
+            .write();
+        }
+    });
+}
+
+function addBoundsToUrl(url) {
+    let bounds = options.areaBounds;
+    Object.keys(bounds).forEach(key => {
+        let val = bounds[key];
+        url += `&${key}=${val}`;
+    })
+    return url;
+}
+
+/*
+ * Convert minutes to miliseconds
+ */
+function minutes(minutes) {
+    return minutes * 1000 * 60;
+}
+
+function log(message) {
+    console.log('\x1b[33m[app] %s\x1b[0m', message);
+}
