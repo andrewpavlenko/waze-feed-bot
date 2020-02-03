28b70dd76871d0d71af97eba16bfa3d7b4748104
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-02-03 13:21:12 +0200

    Added construction handler

--- alerts.js
+++ alerts.js
@@ -3,24 +3,48 @@ const tg = require('./telegram');
 const channelId = process.env.CHANNEL_ID;
 
 const alertTypes = {
-    potHole: 'HAZARD_ON_ROAD_POT_HOLE'
+    potHole: 'HAZARD_ON_ROAD_POT_HOLE',
+    construction: 'HAZARD_ON_ROAD_CONSTRUCTION'
 };
 
 function handleAlert(alert) {
     switch (alert.subtype) {
         case alertTypes.potHole:
             return handlePotHoleAlert(alert);
+        case alertTypes.construction:
+            return handleConstructionAlert(alert);
         default:
             tg.sendUnknownAlertInfo(alert);
     }
 }
 
-function handlePotHoleAlert(alert) {
-    let { reportBy, street, location } = alert;
+function handleConstructionAlert(alert) {
+    let { reportBy, street, city, location } = alert;
     let who = reportBy ? reportBy : 'Хтось';
+    let where = street ? `на ${street}` : `у м. ${city}`;
 
-    let message = `📢 ${who} повідомляє, що на ${street} яма 🙂`;
+    let message = `📢 ${who} повідомляє, що ${where} ремонт дороги 🚧`;
+    let inlineKeyboard = buildLinkReplyKeyboard(location);
 
+    tg.sendMessage(channelId, message, inlineKeyboard);
+}
+
+function handlePotHoleAlert(alert) {
+    let { reportBy, street, city, location } = alert;
+    let who = reportBy ? reportBy : 'Хтось';
+    let where = street ? `на ${street}` : `у м. ${city}`;
+
+    let message = `📢 ${who} повідомляє, що ${where} яма 🙂`;
+    let inlineKeyboard = buildLinkReplyKeyboard(location);
+
+    tg.sendMessage(channelId, message, inlineKeyboard);
+}
+
+function getAlertUrl(location) {
+    return `https://www.waze.com/en/livemap/directions?latlng=${location.y}%2C${location.x}&utm_campaign=waze_website&utm_source=waze_website`;
+}
+
+function buildLinkReplyKeyboard(location) {
     let inlineKeyboard = {
         inline_keyboard: [
             [{
@@ -28,13 +52,8 @@ function handlePotHoleAlert(alert) {
                 url: getAlertUrl(location)
             }]
         ]
-    }
-
-    tg.sendMessage(channelId, message, inlineKeyboard);
-}
-
-function getAlertUrl(location) {
-    return `https://www.waze.com/en/livemap/directions?latlng=${location.y}%2C${location.x}&utm_campaign=waze_website&utm_source=waze_website`;
+    };
+    return inlineKeyboard;
 }
 
 exports.handleAlert = handleAlert;
